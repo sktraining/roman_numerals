40a41668829ca0e0941bfe9b031f856091878c8c
levi serebryanski <levsero@gmail.com>
2015-12-28 11:05:28 -0500

    Add rest of roman numeral chars

--- levi/roman_numerals.rb
+++ levi/roman_numerals.rb
@@ -2,7 +2,11 @@ module RomanNumerals
   ROMAN_NUMERALS = {
     'I' => 1,
     'V' => 5,
-    'X' => 10
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
   }
 
   def self.convert_roman_to_arabic(roman_numeral)
--- levi/roman_numerals_spec.rb
+++ levi/roman_numerals_spec.rb
@@ -30,5 +30,25 @@ describe RomanNumerals do
       let(:input) { 'X' }
       specify { expect(described_class.convert_roman_to_arabic(input)).to eq(10) }
     end
+
+    context 'given L' do
+      let (:input) { 'L' }
+      specify { expect(described_class.convert_roman_to_arabic(input)).to eq(50) }
+    end
+
+    context 'given C' do
+      let (:input) { 'C' }
+      specify { expect(described_class.convert_roman_to_arabic(input)).to eq(100) }
+    end
+
+    context 'given D' do
+      let (:input) { 'D' }
+      specify { expect(described_class.convert_roman_to_arabic(input)).to eq(500) }
+    end
+
+    context 'given M' do
+      let (:input) { 'M' }
+      specify { expect(described_class.convert_roman_to_arabic(input)).to eq(1000) }
+    end
   end
 end
