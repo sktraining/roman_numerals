3321d174f6da467a46994ae85dfcf584bad4d8c5
Patricia Carbajal <carbajal.patricia@gmail.com>
2016-01-15 17:49:02 -0500

    WIP invalid csv specs

--- patricia/csv_handler.rb
+++ patricia/csv_handler.rb
@@ -1,15 +1,43 @@
+require 'csv'
+
 class CSVHandler
-  def initialize(converter)
-    @converter = converter
+  INVALID_CSV_ERROR = "input must be a valid csv file"
+
+  def initialize(converter_klass)
+    @converter_klass = converter_klass
   end
 
   def write_converted(input_csv_path)
+    validate_proper_encoding(input_csv_path)
     output_csv = CSV.generate do |csv|
         CSV.foreach(input_csv_path) do |row|
-          input_value = CSV.parse(row)[0]
-          converted_value = @converter.new(input_value).convert
+          validate_csv_row(row)
+          converted_value = @converter_klass.new(CSV.parse(row)[0][0]).convert
           csv << [converted_value]
         end
       end
+  end
+
+  private
+
+# def csv?(filename, sep: ';', columns: 3)
+#   return false unless File.exist?(filename) #"No file"
+#   content = File.read(filename, :encoding => 'utf-8')
+#   return false unless content.valid_encoding? #"Wrong encoding"
+
+#   content.each_line{|line|
+#     return false if line.count(sep) < columns - 1
+#   }
+#   CSV.parse(content, :col_sep => sep)
+
+# end
+
+  def validate_proper_encoding(filename)
+    content = File.read(filename, :encoding => 'utf-8')
+    raise INVALID_CSV_ERROR  unless content.valid_encoding?
+  end
+
+  def validate_csv_row(row)
+    raise INVALID_CSV_ERROR if CSV.parse(row)[0].count != 1
   end
 end
--- patricia/specs/csv_handler_spec.rb
+++ patricia/specs/csv_handler_spec.rb
@@ -1,10 +1,15 @@
 require_relative '../csv_handler.rb'
+require_relative '../roman_to_arabic_converter.rb'
 
 describe 'csv handler' do
+
+  let(:handler) { CSVHandler.new(RomanToArabicConverter) }
+
   context 'csv file is valid' do
     before do
-      allow(CSV).to receive(:read).with(input_csv_path) { input_csv }
+      allow(CSV).to receive(:foreach).with(input_csv_path).and_yield('X').and_yield('IV').and_yield('CIX').and_yield('M')
     end
+
 
     context 'roman to arabic case' do
 
@@ -17,15 +22,14 @@ describe 'csv handler' do
           end
         end
       end
-      let(:ouput_csv_array) { ['10', '9', '109', '1000'] }
-      let(:ouput_csv) do
+      let(:output_csv_array) { ['10', '4', '109', '1000'] }
+      let(:output_csv) do
         CSV.generate do |csv|
           output_csv_array.each do |arabic|
             csv << [arabic]
           end
         end
       end
-      let(:handler) { CSVHandler.new(RomanToArabicConverter.new) }
 
       it 'csv file contains converted arabic values' do
         expect(handler.write_converted(input_csv_path)).to eq(output_csv)
@@ -34,6 +38,21 @@ describe 'csv handler' do
   end
 
   context 'csv file is invalid' do
+    let(:invalid_input_csv_path) { './invalid_input_csv_path' }
 
+    context "row has more than one cell" do
+      before { allow(CSV).to receive(:foreach).with(invalid_input_csv_path).and_yield('X, I') }
+
+      let(:invalid_input_csv) { [["X", "I"]] }
+
+      specify { expect { handler.write_converted(invalid_input_csv_path) }.to raise_error("input must be a valid csv file") }
+    end
+
+    context "csv has invalid encoding" do
+      let(:invalid_utf_8) { 128.chr }
+      before { allow(File).to receive(:read).with(invalid_input_csv_path, :encoding => 'utf-8') { invalid_utf_8 } }
+
+      specify { expect { handler.write_converted(invalid_input_csv_path) }.to raise_error("input must be a valid csv file") }
+    end
   end
 end
