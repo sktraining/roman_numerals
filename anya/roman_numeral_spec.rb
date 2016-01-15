ef3859c033abeb840121e76d14d61abbaacea40c
Anya Glowa-Kollisch <anya@shopkeep.com>
2016-01-15 16:29:34 -0500

    rename methods, add method alias, add in-between roman nums

--- anya/roman_numeral_spec.rb
+++ anya/roman_numeral_spec.rb
@@ -1,36 +1,41 @@
 require_relative 'roman_numeral'
 
 describe RomanNumeral do
- describe '#convert' do
+ describe '#int_value' do
     context 'when the input is nil' do
-      specify { expect(described_class.new(nil).convert).to eq(0) }
+      specify { expect(described_class.new(nil).int_value).to eq(0) }
     end
     context 'when the input is blank' do
-      specify { expect(described_class.new('').convert).to eq(0) }
+      specify { expect(described_class.new('').int_value).to eq(0) }
     end
     context 'when the input is invalid' do
-      specify { expect { described_class.new('P').convert }.to raise_exception(ArgumentError, "invalid input") }
+      specify { expect { described_class.new('P').int_value }.to raise_exception(ArgumentError, "invalid input") }
     end
     context 'when the input is lowercase' do
-      specify { expect(described_class.new('i').convert).to eq(1) }
+      specify { expect(described_class.new('i').int_value).to eq(1) }
     end
     context 'when the input is valid' do
-      specify { expect(described_class.new('I').convert).to eq(1) }
-      specify { expect(described_class.new('II').convert).to eq(2) }
-      specify { expect(described_class.new('III').convert).to eq(3) }
-      specify { expect(described_class.new('IV').convert).to eq(4) }
-      specify { expect(described_class.new('V').convert).to eq(5) }
-      specify { expect(described_class.new('VI').convert).to eq(6) }
-      specify { expect(described_class.new('VII').convert).to eq(7) }
-      specify { expect(described_class.new('VIII').convert).to eq(8) }
-      specify { expect(described_class.new('IX').convert).to eq(9) }
-      specify { expect(described_class.new('X').convert).to eq(10) }
-      specify { expect(described_class.new('XVI').convert).to eq(16) }
-      specify { expect(described_class.new('LX').convert).to eq(60) }
-      specify { expect(described_class.new('CCXXVIII').convert).to eq(228) }
-      specify { expect(described_class.new('DCLXXIX').convert).to eq(679) }
-      specify { expect(described_class.new('MLIV').convert).to eq(1054) }
+      specify { expect(described_class.new('I').int_value).to eq(1) }
+      specify { expect(described_class.new('II').int_value).to eq(2) }
+      specify { expect(described_class.new('III').int_value).to eq(3) }
+      specify { expect(described_class.new('IV').int_value).to eq(4) }
+      specify { expect(described_class.new('V').int_value).to eq(5) }
+      specify { expect(described_class.new('VI').int_value).to eq(6) }
+      specify { expect(described_class.new('VII').int_value).to eq(7) }
+      specify { expect(described_class.new('VIII').int_value).to eq(8) }
+      specify { expect(described_class.new('IX').int_value).to eq(9) }
+      specify { expect(described_class.new('X').int_value).to eq(10) }
+      specify { expect(described_class.new('XVI').int_value).to eq(16) }
+      specify { expect(described_class.new('LX').int_value).to eq(60) }
+      specify { expect(described_class.new('CCXXVIII').int_value).to eq(228) }
+      specify { expect(described_class.new('DCLXXIX').int_value).to eq(679) }
+      specify { expect(described_class.new('MLIV').int_value).to eq(1054) }
     end
+  end
+
+  describe '#to_i' do
+    subject { described_class.new('X') }
+    specify { expect(subject.method(:to_i)).to eq(subject.method(:int_value)) }
   end
 
   describe '.from_i' do
@@ -52,7 +57,7 @@ describe RomanNumeral do
       specify { expect(described_class.from_i(60).to_s).to eq('LX') }
     end
   end
-  
+
   describe '#to_s' do
     specify { expect(described_class.new('I').to_s).to eq('I') }
   end
