f9e855e9a60bd02828e3ff7425700efbf6c1a6ab
Patricia Carbajal <carbajal.patricia@gmail.com>
2016-01-15 15:45:07 -0500

    split class into two seperate converters

--- patricia/specs/arabic_to_roman_converter_spec.rb
+++ patricia/specs/arabic_to_roman_converter_spec.rb
@@ -1,26 +1,26 @@
 require_relative "../arabic_to_roman_converter.rb"
 
-describe "Arabic to Roman Converter" do
+describe ArabicToRomanConverter do
   context "arabic numeral is valid" do
     context "arabic numbers that convert to single roman numeral" do
-      specify { expect(ArabicToRomanConverter.new(1).convert).to eq("I") }
-      specify { expect(ArabicToRomanConverter.new(5).convert).to eq("V") }
-      specify { expect(ArabicToRomanConverter.new(10).convert).to eq("X") }
-      specify { expect(ArabicToRomanConverter.new(50).convert).to eq("L") }
-      specify { expect(ArabicToRomanConverter.new(100).convert).to eq("C") }
-      specify { expect(ArabicToRomanConverter.new(500).convert).to eq("D") }
-      specify { expect(ArabicToRomanConverter.new(1000).convert).to eq("M") }
+      specify { expect(described_class.new(1).convert).to eq("I") }
+      specify { expect(described_class.new(5).convert).to eq("V") }
+      specify { expect(described_class.new(10).convert).to eq("X") }
+      specify { expect(described_class.new(50).convert).to eq("L") }
+      specify { expect(described_class.new(100).convert).to eq("C") }
+      specify { expect(described_class.new(500).convert).to eq("D") }
+      specify { expect(described_class.new(1000).convert).to eq("M") }
     end
     context "arabic number converts to multiple roman numerals" do
-      specify { expect(ArabicToRomanConverter.new(1944).convert).to eq("MCMXLIV")}
-      specify { expect(ArabicToRomanConverter.new(4321).convert).to eq("MMMMCCCXXI")}
+      specify { expect(described_class.new(1944).convert).to eq("MCMXLIV")}
+      specify { expect(described_class.new(4321).convert).to eq("MMMMCCCXXI")}
     end
   end
 
   context "arabic numeral is invalid" do
-    specify { expect{ ArabicToRomanConverter.new('ABC').convert }.to raise_error("input must be a positive integer") }
-    specify { expect{ ArabicToRomanConverter.new(2.4).convert }.to raise_error("input must be a positive integer") }
-    specify { expect{ ArabicToRomanConverter.new(-10).convert }.to raise_error("input must be a positive integer") }
-    specify { expect{ ArabicToRomanConverter.new(0).convert }.to raise_error("input must be a positive integer") }
+    specify { expect{ described_class.new('ABC').convert }.to raise_error("input must be a positive integer") }
+    specify { expect{ described_class.new(2.4).convert }.to raise_error("input must be a positive integer") }
+    specify { expect{ described_class.new(-10).convert }.to raise_error("input must be a positive integer") }
+    specify { expect{ described_class.new(0).convert }.to raise_error("input must be a positive integer") }
   end
 end
--- patricia/specs/roman_to_arabic_converter_spec.rb
+++ patricia/specs/roman_to_arabic_converter_spec.rb
@@ -1,38 +1,38 @@
 require_relative "../roman_to_arabic_converter.rb"
 
-describe "Roman to Arabic Numeral Converter" do
+describe RomanToArabicConverter do
   context "roman numeral is valid" do
     context "roman numeral is one character" do
-      specify { expect(RomanToArabicConverter.new("I").convert).to eq(1) }
-      specify { expect(RomanToArabicConverter.new("V").convert).to eq(5) }
-      specify { expect(RomanToArabicConverter.new("X").convert).to eq(10) }
-      specify { expect(RomanToArabicConverter.new("L").convert).to eq(50) }
-      specify { expect(RomanToArabicConverter.new("C").convert).to eq(100) }
-      specify { expect(RomanToArabicConverter.new("D").convert).to eq(500) }
-      specify { expect(RomanToArabicConverter.new("M").convert).to eq(1000) }
+      specify { expect(described_class.new("I").convert).to eq(1) }
+      specify { expect(described_class.new("V").convert).to eq(5) }
+      specify { expect(described_class.new("X").convert).to eq(10) }
+      specify { expect(described_class.new("L").convert).to eq(50) }
+      specify { expect(described_class.new("C").convert).to eq(100) }
+      specify { expect(described_class.new("D").convert).to eq(500) }
+      specify { expect(described_class.new("M").convert).to eq(1000) }
     end
     context "addition between roman numerals" do
-      specify { expect(RomanToArabicConverter.new("CXI").convert).to eq(111) }
-      specify { expect(RomanToArabicConverter.new("CCC").convert).to eq(300) }
-      specify { expect(RomanToArabicConverter.new("DIX").convert).to eq(509) }
+      specify { expect(described_class.new("CXI").convert).to eq(111) }
+      specify { expect(described_class.new("CCC").convert).to eq(300) }
+      specify { expect(described_class.new("DIX").convert).to eq(509) }
     end
     context "roman numerals that are two characters long" do
-      specify { expect(RomanToArabicConverter.new("IV").convert).to eq(4) }
-      specify { expect(RomanToArabicConverter.new("IX").convert).to eq(9) }
-      specify { expect(RomanToArabicConverter.new("XL").convert).to eq(40) }
-      specify { expect(RomanToArabicConverter.new("XC").convert).to eq(90) }
-      specify { expect(RomanToArabicConverter.new("CD").convert).to eq(400) }
-      specify { expect(RomanToArabicConverter.new("CM").convert).to eq(900) }
+      specify { expect(described_class.new("IV").convert).to eq(4) }
+      specify { expect(described_class.new("IX").convert).to eq(9) }
+      specify { expect(described_class.new("XL").convert).to eq(40) }
+      specify { expect(described_class.new("XC").convert).to eq(90) }
+      specify { expect(described_class.new("CD").convert).to eq(400) }
+      specify { expect(described_class.new("CM").convert).to eq(900) }
     end
     context "roman numerals that are complex" do
-      specify { expect(RomanToArabicConverter.new("MCMXLIV").convert).to eq(1944)}
-      specify { expect(RomanToArabicConverter.new("MMMMCCCXXI").convert).to eq(4321)}
+      specify { expect(described_class.new("MCMXLIV").convert).to eq(1944)}
+      specify { expect(described_class.new("MMMMCCCXXI").convert).to eq(4321)}
     end
   end
 
   context "roman numeral is invalid" do
-    specify { expect{ RomanToArabicConverter.new(123).convert }.to raise_error("input must be a string") }
-    specify { expect{ RomanToArabicConverter.new("XIQ").convert }.to raise_error("invalid roman input") }
-    specify { expect{ RomanToArabicConverter.new("BOGUS").convert }.to raise_error("invalid roman input") }
+    specify { expect{ described_class.new(123).convert }.to raise_error("input must be a string") }
+    specify { expect{ described_class.new("XIQ").convert }.to raise_error("invalid roman input") }
+    specify { expect{ described_class.new("BOGUS").convert }.to raise_error("invalid roman input") }
   end
 end
