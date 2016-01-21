1261fcbd7ba54921929db3f172995a0dcfae853b
Patricia Carbajal <carbajal.patricia@gmail.com>
2016-01-21 15:33:42 -0500

    began spec skeleton for aws_handler class

---
+++ patricia/specs/aws_handler_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../aws_handler.rb'
+
+describe AWSHandler do
+
+  describe '#config' do
+
+  end
+
+  describe '#read_file' do
+
+    it 'returns the contents of the aws file' do
+
+    end
+  end
+
+  describe '#write_file' do
+
+    it 'writes a file to aws' do
+
+    end
+  end
+end
