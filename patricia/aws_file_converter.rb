d51a1a3f445720d8572acbc450982b3f41553635
Patricia Carbajal <carbajal.patricia@gmail.com>
2016-03-31 17:03:12 -0400

    chapter 3 wip

--- patricia/aws_file_converter.rb
+++ patricia/aws_file_converter.rb
@@ -1,7 +1,10 @@
+require_relative 'aws_handler'
+require_relative 'csv_handler'
+
 class AWSFileConverter
   def init(from_bucket: , to_bucket:, from_key:, to_key:, converter_klass:)
-    # csv_to_convert = AWSHandler.instance.read_file(from_bucket, from_key)
-    # converted_csv = CSVHandler.new(converter).convert_csv(csv_to_convert)
-    # AWSHandler.instance.write_file(to_bucket, to_key, converted_csv)
+    csv_to_convert = AWSHandler.instance.read_file(from_bucket, from_key)
+    converted_csv = CSVHandler.new(converter).convert_csv(csv_to_convert)
+    AWSHandler.instance.write_file(to_bucket, to_key, converted_csv)
   end
 end
---
+++ patricia/specs/aws_file_converter_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../aws_handler'
+require_relative '../csv_handler'
+
+describe AWSFileConverter do
+  describe '#init' do
+  end
+end
